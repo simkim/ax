e25e7f9c3e3691dc6bd244fb0cf4021ce275a680
Gauthier Monserand <simkim@simkim.net>
2013-03-07 18:50:30 +0100

    initial commit

---
+++ servo/config.py
@@ -0,0 +1,39 @@
+import ConfigParser
+
+class ServoConfig(ConfigParser.SafeConfigParser):
+	def __init__(self, filename="ax.ini"):
+		ConfigParser.SafeConfigParser.__init__(self)
+		self.readfp(open(filename))
+		ids = self.get("net", "id")
+		self.ids = []
+		for _id in ids.split(","):
+			_id = int(_id)
+			self.ids.append(_id)
+		self.load_config_for_servos()
+
+	def get_config_for_servo_id(self, ident):
+		config = self.servo_by_id.get(int(ident))
+		if not config:
+			config = self.get_config_for_section("ax.unknown")
+			config["id"] = ident
+		return config
+
+	def load_config_for_servos(self):
+		self.servo_by_id = {}
+		self.servo_by_label = {}
+		for section in self.sections():
+			if not section.startswith("ax."):
+				continue
+			config = self.get_config_for_section(section)
+			self.servo_by_id[config["id"]] = config
+			self.servo_by_label[config["label"]] = config
+		return self.servo_by_id.values()
+			
+	def get_config_for_section(self, section):
+		if section not in self.sections():
+			config = { "label" : "unknown"}
+			return config 
+		return {
+			"label" : self.get(section, "label") or "",
+			"id" : int(section[3:])
+		}
---
+++ servo/network.py
@@ -0,0 +1,23 @@
+import dynamixel
+import serial
+
+class ServoNetwork:
+	def __init__(self, config, exit_on_error=True):
+		baudrate = config.get("net", "baudrate")
+		port = config.get("net", "port")
+		try:
+			s = dynamixel.SerialStream(port=port, baudrate=baudrate, timeout=1)
+		except serial.SerialException, e:
+			print e
+			if exit_on_error:
+				import sys
+				sys.exit(1)
+			else:
+				raise
+		self.net = dynamixel.DynamixelNetwork(s)
+		self.config = config
+	def get_servo(self, ident):
+		d = dynamixel.Dynamixel(ident, self.net)
+		servo_config = self.config.get_config_for_servo_id(ident)
+		d.config = servo_config
+		return d
